6f20b6d0406616f7047112210c7838d0e56c7e80
Reeja Sugathan <Reeja Sugathan@DESKTOP-S02LODF>
2023-04-10 10:44:52 +0530

    1st commit

---
+++ java_programs/src/sampleprograms/Hello.java
@@ -0,0 +1,16 @@
+package sampleprograms;
+
+public class Hello 
+{
+	public void display()
+	{
+		System.out.println("HELLO WORLD");
+	}
+
+	public static void main(String[] args) 
+	{
+		Hello obj1=new Hello();
+		obj1.display();
+	}
+
+}
